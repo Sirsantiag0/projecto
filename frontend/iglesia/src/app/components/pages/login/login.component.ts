7ea7a97879ea0d9b1c6663278f066fe67927a0a9
Sirsantiag0 <sirsantiago@outlook.es>
2025-08-30 18:37:24 -0500

    cambios en login

--- frontend/iglesia/src/app/components/pages/login/login.component.ts
+++ frontend/iglesia/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
@@ -10,9 +10,11 @@ import { AuthService } from '../../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
   @Output() closed = new EventEmitter<void>();
+  @ViewChild('correoInput') correoInput!: ElementRef<HTMLInputElement>;
   form: FormGroup;
+  loading = false;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.form = this.fb.group({
@@ -21,15 +23,23 @@ export class LoginComponent {
     });
   }
 
+    ngAfterViewInit() {
+    this.correoInput.nativeElement.focus();
+  }
+
+
   submit() {
     const { correo, password } = this.form.value;
     if (!correo || !password) return;
+    this.loading = true;
+    setTimeout(() => {
+    this.loading = false;
+    this.closed.emit();
+    }, 1000);
     this.authService.login(correo, password).subscribe({
-      next: () => this.closed.emit(),
       error: err => console.error('Login error', err)
     });
   }
-
   close() {
     this.closed.emit();
   }
