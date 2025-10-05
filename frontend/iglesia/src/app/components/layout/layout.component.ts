ec8c933f68c14cbea02e34ff9ea97625648cee00
Sirsantiag0 <sirsantiago@outlook.es>
2025-10-05 15:03:25 -0500

    relisto

--- frontend/iglesia/src/app/components/layout/layout.component.ts
+++ frontend/iglesia/src/app/components/layout/layout.component.ts
@@ -25,28 +25,6 @@ export class LayoutComponent {
     this.showLogin.set(false);
   }
 
-    canAccessAsistente() {
-    const user = this.authService.user();
-    return !!user && (user.id_rol === 1 || user.id_rol === 2);
-  }
-
-  goToAsistente() {
-    const user = this.authService.user();
-
-    if (!user) {
-      this.openLogin();
-      return;
-    }
-
-    if (!this.canAccessAsistente()) {
-      this.router.navigate(['/login']);
-      return;
-    }
-
-    this.router.navigate(['/asistente']);
-  }
-
-
   onUserOption(event: Event) {
     const value = (event.target as HTMLSelectElement).value;
         if (value === 'mis-eventos') {
